7068cc5325ec7acc9fe1c5298a2187bbbc954e70
Dami34 <damisi.odumosu03@gmail.com>
2025-02-15 15:09:42 -0500

    Add a Register functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     # path for logout
     path(route='logout',  view=views.logout_request, name="logout"),
     
-    # # path for registration
+    # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
